063317d02f790422c8fb28c38f4db0e1636c55d3
mab <marcariza2@gmail.com>
2024-02-13 23:03:14 +0100

    Actualización B 13/02/2024. Renovado script de instalación.

--- install.sh
+++ install.sh
@@ -24,15 +24,39 @@ selectOption() {
 }
 
 installToRoot() {
-	ls ./sway;
+	echo "Se necesitarán permisos de superusuario..." 
+	echo "Copiando configuración de Sway... " && 
+		sudo mkdir -p /etc/xdg/sway &> /dev/null &&
+		sudo cp ./sway/* /etc/xdg/sway
+	echo "Copiando configuración de Waybar..." &&
+	 	sudo mkdir -p /etc/xdg/waybar &> /dev/null &&	
+		sudo cp ./waybar/* ~/.config/waybar
+	echo "Copiando configuración de Fuzzel..." &&
+	 	sudo mkdir -p /etc/xdg/fuzzel &> /dev/null &&	
+		sudo cp ./fuzzel/* ~/.config/fuzzel
+	echo "Copiando configuración de Foot Terminal..." &&
+	 	sudo mkdir -p /etc/xdg/foot &> /dev/null &&	
+		sudo cp ./foot/* ~/.config/foot
+	echo "Copiando .bashrc a la carpeta del usuario..." &&
+	       	sudo cp ./.bashrc ~
+	echo "¡Terminado!"
 }
 
 installToUser() {
-	echo "Copiando configuración de Sway... " && cp ./sway/* ~/.config/sway
-	echo "Copiando configuración de Waybar..." && cp ./waybar/* ~/.config/waybar
-	echo "Copiando configuración de Fuzzel..." && cp ./fuzzel/* ~/.config/fuzzel
-	echo "Copiando configuración de Foot Terminal..." && cp ./foot/* ~/.config/foot
-	echo "Copiando .bashrc a la carpeta del usuario..." && cp ./.bashrc ~
+	echo "Copiando configuración de Sway... " &&
+	       	mkdir -p ~/.config/sway	&> /dev/null &&
+	       	cp ./sway/* ~/.config/sway  
+	echo "Copiando configuración de Waybar..." &&
+	       	mkdir -p ~/.config/waybar &> /dev/null &&
+	       	cp ./waybar/* ~/.config/waybar
+	echo "Copiando configuración de Fuzzel..." &&
+	       	mkdir -p ~/.config/fuzzel &> /dev/null &&
+	       	cp ./fuzzel/* ~/.config/fuzzel 
+	echo "Copiando configuración de Foot Terminal..." &&
+	       	mkdir -p ~/.config/foot &> /dev/null &&
+		cp ./foot/* ~/.config/foot 
+	echo "Copiando .bashrc a la carpeta del usuario..." &&
+	       	cp ./.bashrc ~
 	echo "¡Terminado!"
 }
 
